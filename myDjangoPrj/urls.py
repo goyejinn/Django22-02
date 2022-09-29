c9d3eea3c9745ffeceb4570a8d019dada4ce0464
Yejin <kyj5840kyj@duksung.ac.kr>
2022-09-29 14:19:06 +0900

    FBV로 대문/상세 페이지 만들기

--- myDjangoPrj/urls.py
+++ myDjangoPrj/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls), # IP 주소/admin
-    path('blog/', include('blog.urls')) # IP 주소/blog
+    path('blog/', include('blog.urls')), # IP 주소/blog
+    path('', include('single_pages.urls')) # IP 주소/
 ]
