d257621dd2df16f38d2adc38764666f9502f0529
Yejin <kyj5840kyj@duksung.ac.kr>
2022-09-29 14:41:45 +0900

    CBV로 포스트 목록/상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [ # IP 주소/blog
-    path('', views.index),  # IP 주소/blog
-    path('<int:pk>/', views.single_post_page)
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view())
+
+    #path('', views.index),  # IP 주소/blog
+    #path('<int:pk>/', views.single_post_page)
 ]
