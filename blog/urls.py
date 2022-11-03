dc6fd00b19a91dbe9544dccccd0a6f6adec42cbd
Yejin <kyj5840kyj@duksung.ac.kr>
2022-11-03 14:44:19 +0900

    다대다 관계 구현하기 완료

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-urlpatterns = [ # IP 주소/blog
+urlpatterns = [ # IP주소/blog
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
-    path('category/<str:slug>/', views.category_page)
+    path('category/<str:slug>/', views.category_page), # IP주소/blog/category/slug
+    path('tag/<str:slug>/', views.tag_page),            # IP주소/blog/tag/slug
 
-    #path('', views.index),  # IP 주소/blog
+    #path('', views.index),  # IP주소/blog
     #path('<int:pk>/', views.single_post_page)
 ]
