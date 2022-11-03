070ec95ecc95cf1d6fae41c4b803358672c5536f
Yejin <kyj5840kyj@duksung.ac.kr>
2022-11-03 13:49:45 +0900

    다대일 관계 구현하기 완료

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 import os
-# Create your models here.
 
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}'
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Post(models.Model) :
     title = models.CharField(max_length=30)
@@ -18,6 +30,8 @@ class Post(models.Model) :
 
     # 추후 author 작성
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
     def __str__(self):
         return f'[{self.pk}]{self.title}:: {self.author} : {self.created_at}'
 
